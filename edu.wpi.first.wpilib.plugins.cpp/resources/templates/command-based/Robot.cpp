a09abcd3c903136117bcab790dc4b68d7174914b
Kevin O'Connor <koconnor@usfirst.org>
2015-12-18 12:36:23 -0500

    Update templates to show autonomous mode selection. Default to SendableChooser, but include info on using SD string from LabVIEW Dashboard.
    Change-Id: I3cd317c67d173c1b21feb3b2f2d4518324151836

--- edu.wpi.first.wpilib.plugins.cpp/resources/templates/command-based/Robot.cpp
+++ edu.wpi.first.wpilib.plugins.cpp/resources/templates/command-based/Robot.cpp
@@ -7,11 +7,15 @@ class Robot: public IterativeRobot
 {
 private:
 	std::unique_ptr<Command> autonomousCommand;
+	SendableChooser *chooser;
 
 	void RobotInit()
 	{
 		CommandBase::init();
-		autonomousCommand.reset(new ExampleCommand());
+		chooser = new SendableChooser();
+		chooser->AddDefault("Default Auto", new ExampleCommand());
+		//chooser->AddObject("My Auto", new MyAutoCommand());
+		SmartDashboard::PutData("Auto Modes", chooser);
 	}
 
 	/**
@@ -28,8 +32,26 @@ private:
 		Scheduler::GetInstance()->Run();
 	}
 
+	/**
+	 * This autonomous (along with the chooser code above) shows how to select between different autonomous modes
+	 * using the dashboard. The sendable chooser code works with the Java SmartDashboard. If you prefer the LabVIEW
+	 * Dashboard, remove all of the chooser code and uncomment the GetString code to get the auto name from the text box
+	 * below the Gyro
+	 *
+	 * You can add additional auto modes by adding additional commands to the chooser code above (like the commented example)
+	 * or additional comparisons to the if-else structure below with additional strings & commands.
+	 */
 	void AutonomousInit()
 	{
+		/* std::string autoSelected = SmartDashboard::GetString("Auto Selector", "Default");
+		if(autoSelected == "My Auto") {
+			autonomousCommand.reset(new MyAutoCommand());
+		} else {
+			autonomousCommand.reset(new ExampleCommand());
+		} */
+
+		autonomousCommand.reset((Command *)chooser->GetSelected());
+
 		if (autonomousCommand != NULL)
 			autonomousCommand->Start();
 	}
--- edu.wpi.first.wpilib.plugins.java/resources/templates/iterative/Robot.java
+++ edu.wpi.first.wpilib.plugins.java/resources/templates/iterative/Robot.java
@@ -2,6 +2,9 @@
 package $package;
 
 import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -11,19 +14,49 @@ import edu.wpi.first.wpilibj.IterativeRobot;
  * directory.
  */
 public class Robot extends IterativeRobot {
+    final String defaultAuto = "Default";
+    final String customAuto = "My Auto";
+    String autoSelected;
+    SendableChooser chooser;
+	
     /**
      * This function is run when the robot is first started up and should be
      * used for any initialization code.
      */
     public void robotInit() {
-
+        chooser = new SendableChooser();
+        chooser.addDefault("Default Auto", defaultAuto);
+        chooser.addObject("My Auto", customAuto);
+    }
+    
+	/**
+	 * This autonomous (along with the chooser code above) shows how to select between different autonomous modes
+	 * using the dashboard. The sendable chooser code works with the Java SmartDashboard. If you prefer the LabVIEW
+	 * Dashboard, remove all of the chooser code and uncomment the getString line to get the auto name from the text box
+	 * below the Gyro
+	 *
+	 * You can add additional auto modes by adding additional comparisons to the switch structure below with additional strings.
+	 * If using the SendableChooser make sure to add them to the chooser code above as well.
+	 */
+    public void autonomousInit() {
+    	autoSelected = (String) chooser.getSelected();
+//		autoSelected = SmartDashboard.getString("Auto Selector", defaultAuto);
+		System.out.println("Auto selected: " + autoSelected);
     }
 
     /**
      * This function is called periodically during autonomous
      */
     public void autonomousPeriodic() {
-
+    	switch(autoSelected) {
+    	case customAuto:
+        //Put custom auto code here   
+            break;
+    	case defaultAuto:
+    	default:
+    	//Put default auto code here
+            break;
+    	}
     }
 
     /**
