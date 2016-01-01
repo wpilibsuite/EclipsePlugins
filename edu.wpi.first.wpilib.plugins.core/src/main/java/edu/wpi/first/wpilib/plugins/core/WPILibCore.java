ddd8f524c182079fa5644ef40112ec6fbbbb7de0
Brad Miller (WPI) <brad@wpi.edu>
2016-01-01 06:22:36 -0800

    Revert "Remove obsolete tools version from Preferences page"
    This reverts commit 72d47edfd6e79680e9dc86bf557b39b23576e44e.
    
    Change-Id: I2e0c2c00019d9e84e3fb6e9a2d05bc242cda3636

--- edu.wpi.first.wpilib.plugins.core/src/main/java/edu/wpi/first/wpilib/plugins/core/WPILibCore.java
+++ edu.wpi.first.wpilib.plugins.core/src/main/java/edu/wpi/first/wpilib/plugins/core/WPILibCore.java
@@ -147,6 +147,11 @@ public class WPILibCore extends AbstractUIPlugin {
 		}
 	}
 
+	public String getCurrentVersion() {
+		return getPreferenceStore()
+				.getString(PreferenceConstants.TOOLS_VERSION);
+	}
+
 	public static void logInfo(String msg) {
 		getDefault().getLog().log(new Status(Status.INFO, PLUGIN_ID, Status.OK, msg, null));
 	}
