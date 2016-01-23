24091e31e4426527069dde526f20d4b7773891cc
Joe Ross <git@rossesmail.com>
2016-01-23 10:07:54 -0800

    Modify examples to use AnalogGyro.
    Examples don't compile as they were. Also fix whitespace issues
    in java gyro examples.
    
    Change-Id: I57fc45aa7f0ebe139ce3e6c4459f193b7dd3817d

--- edu.wpi.first.wpilib.plugins.java/resources/templates/examples/Gyro/src/org/usfirst/frc/team190/robot/Robot.java
+++ edu.wpi.first.wpilib.plugins.java/resources/templates/examples/Gyro/src/org/usfirst/frc/team190/robot/Robot.java
@@ -1,17 +1,17 @@
 
 package $package;
 
+import edu.wpi.first.wpilibj.AnalogGyro;
 import edu.wpi.first.wpilibj.CANTalon;
-import edu.wpi.first.wpilibj.Gyro;
 import edu.wpi.first.wpilibj.SampleRobot;
 import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.Joystick;
 
 /**
- * This is a sample program to demonstrate how to use a gyro sensor to make a robot drive 
+ * This is a sample program to demonstrate how to use a gyro sensor to make a robot drive
  * straight. This program uses a joystick to drive forwards and backwards while the gyro
  * is used for direction keeping.
- * 
+ *
  * WARNING: While it may look like a good choice to use for your code if you're inexperienced,
  * don't. Unless you know what you are doing, complex code will be much more difficult under
  * this system. Use IterativeRobot or Command-Based instead if you're new.
@@ -20,39 +20,39 @@ public class Robot extends SampleRobot {
 
     final int gyroChannel = 0; //analog input
     final int joystickChannel = 0; //usb number in DriverStation
-    
+
     //channels for motors
     final int leftMotorChannel = 1;
     final int rightMotorChannel = 0;
     final int leftRearMotorChannel = 3;
     final int rightRearMotorChannel = 2;
-    
+
     double angleSetpoint = 0.0;
     final double pGain = .006; //propotional turning constant
-    
-    //gyro calibration constant, may need to be adjusted; 
+
+    //gyro calibration constant, may need to be adjusted;
     //gyro value of 360 is set to correspond to one full revolution
-    final double voltsPerDegreePerSecond = .0128; 
-      	
+    final double voltsPerDegreePerSecond = .0128;
+
     RobotDrive myRobot;
-    Gyro gyro;
+    AnalogGyro gyro;
     Joystick joystick;
 
     public Robot()
     {
-	//make objects for the drive train, gyro, and joystick
-	myRobot = new RobotDrive(new CANTalon(leftMotorChannel), new CANTalon(
-		leftRearMotorChannel), new CANTalon(rightMotorChannel),
-		new CANTalon(rightRearMotorChannel));
-	gyro = new Gyro(gyroChannel);
-	joystick = new Joystick(joystickChannel);
+        //make objects for the drive train, gyro, and joystick
+        myRobot = new RobotDrive(new CANTalon(leftMotorChannel), new CANTalon(
+          leftRearMotorChannel), new CANTalon(rightMotorChannel),
+          new CANTalon(rightRearMotorChannel));
+        gyro = new AnalogGyro(gyroChannel);
+        joystick = new Joystick(joystickChannel);
     }
 
     /**
      * Runs during autonomous.
      */
     public void autonomous() {
-     
+
     }
 
     /**
@@ -61,18 +61,18 @@ public class Robot extends SampleRobot {
      * from the error between the setpoint and the gyro angle.
      */
     public void operatorControl() {
-	double turningValue;
-	gyro.setSensitivity(voltsPerDegreePerSecond); //calibrates gyro values to equal degrees
+        double turningValue;
+        gyro.setSensitivity(voltsPerDegreePerSecond); //calibrates gyro values to equal degrees
         while (isOperatorControl() && isEnabled()) {
-            
+
             turningValue =  (angleSetpoint - gyro.getAngle())*pGain;
             if(joystick.getY() <= 0)
             {
-        	//forwards
-        	myRobot.drive(joystick.getY(), turningValue); 
+                //forwards
+                myRobot.drive(joystick.getY(), turningValue);
             } else {
-        	//backwards
-        	myRobot.drive(joystick.getY(), -turningValue); 
+                //backwards
+                myRobot.drive(joystick.getY(), -turningValue);
             }
         }
     }
@@ -81,6 +81,6 @@ public class Robot extends SampleRobot {
      * Runs during test mode.
      */
     public void test(){
-	
+
     }
 }
