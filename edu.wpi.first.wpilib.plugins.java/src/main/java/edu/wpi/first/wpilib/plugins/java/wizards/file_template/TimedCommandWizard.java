b820f2362c62946c99692de726a1e50d923984da
sciencewhiz <git@rossesmail.com>
2016-12-29 13:58:45 -0800

    [WIP] Add new commands to Eclipse wizards

---
+++ edu.wpi.first.wpilib.plugins.java/src/main/java/edu/wpi/first/wpilib/plugins/java/wizards/file_template/TimedCommandWizard.java
@@ -0,0 +1,9 @@
+package edu.wpi.first.wpilib.plugins.java.wizards.file_template;
+
+public class TimedCommandWizard extends FileTemplateWizard {
+
+	public TimedCommandWizard() {
+		super("TimedCommand", "command-based/TimedCommand.java", "commands");
+	}
+	
+}
