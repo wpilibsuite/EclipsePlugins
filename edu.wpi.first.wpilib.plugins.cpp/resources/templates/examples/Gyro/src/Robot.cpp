7140c2639b7c726e07ff4d8a11eade3b201d4b5b
Austin Shalit <austinshalit@gmail.com>
2016-12-29 16:55:54 -0700

    Remove CANTalon from Gyro example

--- edu.wpi.first.wpilib.plugins.cpp/resources/templates/examples/Gyro/src/Robot.cpp
+++ edu.wpi.first.wpilib.plugins.cpp/resources/templates/examples/Gyro/src/Robot.cpp
@@ -1,81 +1,42 @@
 #include <AnalogGyro.h>
-#include <CANTalon.h>
 #include <Joystick.h>
 #include <RobotDrive.h>
-#include <SampleRobot.h>
+#include <IterativeRobot.h>
+#include <math.h>
 
 /**
  * This is a sample program to demonstrate how to use a gyro sensor to make a robot drive
  * straight. This program uses a joystick to drive forwards and backwards while the gyro
  * is used for direction keeping.
- *
- * WARNING: While it may look like a good choice to use for your code if you're inexperienced,
- * don't. Unless you know what you are doing, complex code will be much more difficult under
- * this system. Use IterativeRobot or Command-Based instead if you're new.
  */
-class Robot: public SampleRobot {
+class Robot: public IterativeRobot {
 public:
-	/**
-	 * Runs during autonomous.
-	 */
-	void Autonomous() {}
-
-	/**
-	 * Sets the gyro sensitivity and drives the robot when the joystick is
-	 * pushed. The motor speed is set from the joystick while the RobotDrive
-	 * turning value is assigned from the error between the setpoint and the
-	 * gyro angle.
-	 */
-	void OperatorControl() {
-		double turningValue;
-
-		// Calibrates gyro values to equal degrees
-		gyro.SetSensitivity(voltsPerDegreePerSecond);
-
-		while (IsOperatorControl() && IsEnabled()) {
-			turningValue = (angleSetpoint - gyro.GetAngle()) * pGain;
-			if (joystick.GetY() <= 0) {
-				// Forwards
-				myRobot.Drive(joystick.GetY(), turningValue);
-			}
-			else {
-				// Backwards
-				myRobot.Drive(joystick.GetY(), -turningValue);
-			}
-		}
+	void robotInit() {
+		gyro.SetSensitivity(VOLTS_PER_DEGREE_PER_SECOND);
 	}
 
 	/**
-	 * Runs during test mode.
+	 * The motor speed is set from the joystick while the RobotDrive turning
+	 * value is assigned from the error between the setpoint and the gyro angle.
 	 */
-	void Test() {}
+	void teleopPeriodic() {
+		double turningValue = (ANGLE_SETPOINT - gyro.GetAngle()) * KP;
+		// Invert the direction of the turn if we are going backwards
+		turningValue = std::copysign(turningValue, joystick.GetY());
+		myRobot.Drive(joystick.GetY(), turningValue);
+	}
 
 private:
-	constexpr int gyroChannel = 0;  // Analog input
-	constexpr int joystickChannel = 0;  // USB number in DriverStation
-
-	// Channels for motors
-	constexpr int leftFontMotorChannel = 1;
-	constexpr int rightFrontMotorChannel = 0;
-	constexpr int leftRearMotorChannel = 3;
-	constexpr int rightRearMotorChannel = 2;
-
-	double angleSetpoint = 0.0;
-	constexpr double pGain = 0.006;  // Propotional turning constant
+	static constexpr double ANGLE_SETPOINT = 0.0;
+	static constexpr double KP = 0.005;  // Propotional turning constant
 
 	// Gyro calibration constant, may need to be adjusted
 	// Gyro value of 360 is set to correspond to one full revolution
-	constexpr double voltsPerDegreePerSecond = 0.0128;
+	static constexpr double VOLTS_PER_DEGREE_PER_SECOND = 0.0128;
 
-	// Create the drivetrain from 4 CAN Talon SRXs.
-	RobotDrive myRobot{new CANTalon(leftFrontMotorChannel),
-	                   new CANTalon(leftRearMotorChannel),
-	                   new CANTalon(rightFrontMotorChannel),
-	                   new CANTalon(rightRearMotorChannel)};
-
-	// Assign the gyro and joystick channels.
-	AnalogGyro gyro{gyroChannel};
-	Joystick joystick{joystickChannel};
+	RobotDrive myRobot{0, 1};
+	AnalogGyro gyro{0};
+	Joystick joystick{0};
 };
 
 START_ROBOT_CLASS(Robot)
--- edu.wpi.first.wpilib.plugins.java/resources/templates/examples/Gyro/src/org/usfirst/frc/team190/robot/Robot.java
+++ edu.wpi.first.wpilib.plugins.java/resources/templates/examples/Gyro/src/org/usfirst/frc/team190/robot/Robot.java
@@ -1,86 +1,45 @@
-
 package $package;
 
 import edu.wpi.first.wpilibj.AnalogGyro;
-import edu.wpi.first.wpilibj.CANTalon;
-import edu.wpi.first.wpilibj.SampleRobot;
+import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.Joystick;
 
 /**
- * This is a sample program to demonstrate how to use a gyro sensor to make a robot drive
- * straight. This program uses a joystick to drive forwards and backwards while the gyro
- * is used for direction keeping.
- *
- * WARNING: While it may look like a good choice to use for your code if you're inexperienced,
- * don't. Unless you know what you are doing, complex code will be much more difficult under
- * this system. Use IterativeRobot or Command-Based instead if you're new.
+ * This is a sample program to demonstrate how to use a gyro sensor to make a
+ * robot drive straight. This program uses a joystick to drive forwards and
+ * backwards while the gyro is used for direction keeping.
  */
-public class Robot extends SampleRobot {
+public class Robot extends IterativeRobot {
 
-    final int gyroChannel = 0; //analog input
-    final int joystickChannel = 0; //usb number in DriverStation
+    private static final double ANGLE_SETPOINT = 0.0;
+    private static final double KP = 0.005; // propotional turning constant
 
-    //channels for motors
-    final int leftMotorChannel = 1;
-    final int rightMotorChannel = 0;
-    final int leftRearMotorChannel = 3;
-    final int rightRearMotorChannel = 2;
+    // gyro calibration constant, may need to be adjusted;
+    // gyro value of 360 is set to correspond to one full revolution
+    private static final double VOLTS_PER_DEGREE_PER_SECOND = .0128;
 
-    double angleSetpoint = 0.0;
-    final double pGain = .006; //propotional turning constant
+    private RobotDrive myRobot;
+    private AnalogGyro gyro;
+    private Joystick joystick;
 
-    //gyro calibration constant, may need to be adjusted;
-    //gyro value of 360 is set to correspond to one full revolution
-    final double voltsPerDegreePerSecond = .0128;
+    public void robotInit() {
+        myRobot = new RobotDrive(0, 1);
+        gyro = new AnalogGyro(0);
+        joystick = new Joystick(0);
 
-    RobotDrive myRobot;
-    AnalogGyro gyro;
-    Joystick joystick;
-
-    public Robot()
-    {
-        //make objects for the drive train, gyro, and joystick
-        myRobot = new RobotDrive(new CANTalon(leftMotorChannel), new CANTalon(
-          leftRearMotorChannel), new CANTalon(rightMotorChannel),
-          new CANTalon(rightRearMotorChannel));
-        gyro = new AnalogGyro(gyroChannel);
-        joystick = new Joystick(joystickChannel);
+        gyro.setSensitivity(VOLTS_PER_DEGREE_PER_SECOND);
     }
 
     /**
-     * Runs during autonomous.
+     * The motor speed is set from the joystick while the RobotDrive turning
+     * value is assigned from the error between the setpoint and the gyro angle.
      */
-    public void autonomous() {
-
+    public void teleopPeriodic() {
+        double turningValue = (ANGLE_SETPOINT - gyro.getAngle()) * KP;
+        // Invert the direction of the turn if we are going backwards
+        turningValue = Math.copySign(turningValue, joystick.getY());
+        myRobot.drive(joystick.getY(), turningValue);
     }
 
-    /**
-     * Sets the gyro sensitivity and drives the robot when the joystick is pushed. The
-     * motor speed is set from the joystick while the RobotDrive turning value is assigned
-     * from the error between the setpoint and the gyro angle.
-     */
-    public void operatorControl() {
-        double turningValue;
-        gyro.setSensitivity(voltsPerDegreePerSecond); //calibrates gyro values to equal degrees
-        while (isOperatorControl() && isEnabled()) {
-
-            turningValue =  (angleSetpoint - gyro.getAngle())*pGain;
-            if(joystick.getY() <= 0)
-            {
-                //forwards
-                myRobot.drive(joystick.getY(), turningValue);
-            } else {
-                //backwards
-                myRobot.drive(joystick.getY(), -turningValue);
-            }
-        }
-    }
-
-    /**
-     * Runs during test mode.
-     */
-    public void test(){
-
-    }
 }
