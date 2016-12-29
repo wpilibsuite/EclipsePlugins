a2be8f04be9921e7e1790543da7ef4379e90d943
Thad House <thadhouse1@gmail.com>
2016-12-28 20:10:08 -0800

    Updates screensteps URL

--- edu.wpi.first.wpilib.plugins.cpp/resources/templates/examples/QuickVision/src/Robot.cpp
+++ edu.wpi.first.wpilib.plugins.cpp/resources/templates/examples/QuickVision/src/Robot.cpp
@@ -5,7 +5,7 @@
  * or frc scope (ex. `frc::RobotDrive`) are required to use WPILib functionality. 
  * For headers, use scope instead of `using namespace frc` to avoid global namespace
  * pollution. For source files, either option is viable.
- * See 
+ * See http://wpilib.screenstepslive.com/s/4485/m/13810/l/680719 for more information
  */
 using namespace frc;
 
