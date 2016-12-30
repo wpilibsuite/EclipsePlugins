0dc7e5e728f0d86c0073394558ec1e1aa4feaa2b
Austin Shalit <austinshalit@gmail.com>
2016-12-29 23:20:18 -0700

    Update QuickVision

--- edu.wpi.first.wpilib.plugins.java/resources/templates/examples/QuickVision/src/org/usfirst/frc/team190/robot/Robot.java
+++ edu.wpi.first.wpilib.plugins.java/resources/templates/examples/QuickVision/src/org/usfirst/frc/team190/robot/Robot.java
@@ -1,35 +1,18 @@
 package $package;
 
 import edu.wpi.first.wpilibj.CameraServer;
-import edu.wpi.first.wpilibj.SampleRobot;
-import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.IterativeRobot;
 
 /**
- * This is a demo program showing the use of the CameraServer class.
- * With start automatic capture, there is no opportunity to process the image.
- * Look at the IntermediateVision sample for how to process the image before sending it to the FRC PC Dashboard.
+ * Uses the CameraServer class to automatically capture video from a USB webcam
+ * and send it to the FRC dashboard without doing any vision processing. This
+ * is the easiest way to get camera images to the dashboard. Just add this to the
+ * robotInit() method in your program.
  */
-public class Robot extends SampleRobot {
+public class Robot extends IterativeRobot {
 
-    CameraServer server;
-
-    public Robot() {
-        server = CameraServer.getInstance();
-        server.setQuality(50);
-        //the camera name (ex "cam0") can be found through the roborio web interface
-        server.startAutomaticCapture("cam0");
-    }
-
-    /**
-     * start up automatic capture you should see the video stream from the
-     * webcam in your FRC PC Dashboard.
-     */
-    public void operatorControl() {
-
-        while (isOperatorControl() && isEnabled()) {
-            /** robot code here! **/
-            Timer.delay(0.005);        // wait for a motor update time
-        }
-    }
+	public void robotInit() {
+		CameraServer.getInstance().startAutomaticCapture();
+	}
 
 }
