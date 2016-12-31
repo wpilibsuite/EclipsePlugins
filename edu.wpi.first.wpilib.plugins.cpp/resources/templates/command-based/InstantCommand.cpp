9f864a55a7e3c7d0c8e8cf085bf3bd0690d388fc
sciencewhiz <git@rossesmail.com>
2016-12-31 15:34:24 -0800

    Add InstantCommand to C++

---
+++ edu.wpi.first.wpilib.plugins.cpp/resources/templates/command-based/InstantCommand.cpp
@@ -0,0 +1,11 @@
+#include "$classname.h"
+
+$classname::$classname() {
+	// Use Requires() here to declare subsystem dependencies
+	// eg. Requires(chassis);
+}
+
+// Called once when the command executes
+void $classname::Initialize() {
+
+}
---
+++ edu.wpi.first.wpilib.plugins.cpp/resources/templates/command-based/InstantCommand.h
@@ -0,0 +1,12 @@
+#ifndef $classname_H
+#define $classname_H
+
+#include "Commands/InstantCommand.h"
+
+class $classname : public InstantCommand {
+public:
+	$classname();
+	void Initialize();
+};
+
+#endif  // $classname_H
