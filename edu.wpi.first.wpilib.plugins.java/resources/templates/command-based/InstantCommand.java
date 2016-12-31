7606b10f56c5d7c97cdc7badf08d3d44f5b8607e
sciencewhiz <git@rossesmail.com>
2016-12-31 09:43:12 -0800

    Add Instant Command for Java

---
+++ edu.wpi.first.wpilib.plugins.java/resources/templates/command-based/InstantCommand.java
@@ -0,0 +1,20 @@
+package $package;
+
+import edu.wpi.first.wpilibj.command.InstantCommand;
+
+/**
+ *
+ */
+public class $classname extends InstantCommand {
+
+    public $classname() {
+        super();
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    }
+
+}
