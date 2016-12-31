a89e8adc957ac14c2f1ec3067558be0c7bfe4822
Austin Shalit <austinshalit@gmail.com>
2016-12-31 00:07:19 -0700

    Update examples to put rectangle on image

--- edu.wpi.first.wpilib.plugins.cpp/resources/templates/examples/IntermediateVision/src/Robot.cpp
+++ edu.wpi.first.wpilib.plugins.cpp/resources/templates/examples/IntermediateVision/src/Robot.cpp
@@ -2,42 +2,40 @@
 #include <IterativeRobot.h>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/core/core.hpp>
+#include <opencv2/core/types.hpp>
 
 /**
  * This is a demo program showing the use of OpenCV to do vision processing. The
- * image is acquired from the USB Webcam, then is converted to grayscale and
+ * image is acquired from the USB camera, then a rectangle is put on the image and
  * sent to the dashboard. OpenCV has many methods for different types of
  * processing.
  */
 class Robot: public IterativeRobot {
 private:
 	static void VisionThread() {
-		// Get the UsbCamera from CameraServer
-		cs::UsbCamera camera =
-				CameraServer::GetInstance()->StartAutomaticCapture();
+		// Get the USB camera from CameraServer
+		cs::UsbCamera camera = CameraServer::GetInstance()->StartAutomaticCapture();
 		// Set the resolution
 		camera.SetResolution(640, 480);
 
-		// Get a CvSink. This will capture Mats from the UsbCamera
+		// Get a CvSink. This will capture Mats from the camera
 		cs::CvSink cvSink = CameraServer::GetInstance()->GetVideo();
 		// Setup a CvSource. This will send images back to the Dashboard
-		cs::CvSource outputStreamStd = CameraServer::GetInstance()->PutVideo(
-				"Gray", 640, 480);
+		cs::CvSource outputStreamStd = CameraServer::GetInstance()->
+				PutVideo("Rectangle", 640, 480);
 
-		// Mats are very memory expensive. Lets reuse these 2 Mats for all
-		// of our operations.
-		cv::Mat source;
-		cv::Mat output;
+		// Mats are very memory expensive. Lets reuse this Mat.
+		cv::Mat mat;
 
 		while (true) {
-			// Tell the CvSink to grab a frame from the UsbCamera and put it
+			// Tell the CvSink to grab a frame from the camera and put it
 			// in the source mat
-			cvSink.GrabFrame(source);
-			// This line converts the source mat to grayscale and saves it
-			// in the output mat
-			cvtColor(source, output, cv::COLOR_BGR2GRAY);
+			cvSink.GrabFrame(mat);
+			// Put a rectangle on the image
+			rectangle(mat, cv::Point(100, 100), cv::Point(400, 400),
+					cv::Scalar(255, 255, 255), 5);
 			// Give the output stream a new image to display
-			outputStreamStd.PutFrame(output);
+			outputStreamStd.PutFrame(mat);
 		}
 	}
 
--- edu.wpi.first.wpilib.plugins.java/resources/templates/examples/AxisCameraSample/src/org/usfirst/frc/team190/robot/Robot.java
+++ edu.wpi.first.wpilib.plugins.java/resources/templates/examples/AxisCameraSample/src/org/usfirst/frc/team190/robot/Robot.java
@@ -6,11 +6,13 @@ import edu.wpi.cscore.CvSource;
 import edu.wpi.first.wpilibj.CameraServer;
 import edu.wpi.first.wpilibj.IterativeRobot;
 import org.opencv.core.Mat;
+import org.opencv.core.Point;
+import org.opencv.core.Scalar;
 import org.opencv.imgproc.Imgproc;
 
 /**
  * This is a demo program showing the use of OpenCV to do vision processing. The
- * image is acquired from the AxisCamera, then is converted to grayscale and
+ * image is acquired from the Axis camera, then a rectangle is put on the image and
  * sent to the dashboard. OpenCV has many methods for different types of
  * processing.
  */
@@ -18,30 +20,31 @@ public class Robot extends IterativeRobot {
 
 	public void robotInit() {
 		new Thread(() -> {
-			// Get the AxisCamera from CameraServer
+			// Get the Axis camera from CameraServer
 			AxisCamera camera = CameraServer.getInstance().addAxisCamera("axis-camera.local");
 			// Set the resolution
 			camera.setResolution(640, 480);
 
-			// Get a CvSink. This will capture Mats from the AxisCamera
+			// Get a CvSink. This will capture Mats from the camera
 			CvSink cvSink = CameraServer.getInstance().getVideo();
 			// Setup a CvSource. This will send images back to the Dashboard
-			CvSource outputStream = CameraServer.getInstance().putVideo("Gray", 640, 480);
+			CvSource outputStream = CameraServer.getInstance().putVideo("Rectangle", 640, 480);
 
-			// Mats are very memory expensive. Lets reuse these 2 Mats for all
-			// of our operations.
-			Mat source = new Mat();
-			Mat output = new Mat();
+			// Mats are very memory expensive. Lets reuse this Mat.
+			Mat mat = new Mat();
 
-			while (true) {
-				// Tell the CvSink to grab a frame from the AxisCamera and put it
+			// This cannot be 'true'. The program will never exit if it is. This
+			// lets the robot stop this thread when restarting robot code or
+			// deploying.
+			while (!Thread.interrupted()) {
+				// Tell the CvSink to grab a frame from the camera and put it
 				// in the source mat
-				cvSink.grabFrame(source);
-				// This line converts the source mat to grayscale and saves it
-				// in the output mat
-				Imgproc.cvtColor(source, output, Imgproc.COLOR_BGR2GRAY);
+				cvSink.grabFrame(mat);
+				// Put a rectangle on the image
+				Imgproc.rectangle(mat, new Point(100, 100), new Point(400, 400),
+						new Scalar(255, 255, 255), 5);
 				// Give the output stream a new image to display
-				outputStream.putFrame(output);
+				outputStream.putFrame(mat);
 			}
 		}).start();
 	}
